463279f16cc171863b26b89b829e50346099d536
richard-deng <richard.deng@live.com>
2017-05-17 10:37:55 +0800

    可以用昵称搜索

--- base/uyu_user.py
+++ base/uyu_user.py
@@ -256,7 +256,9 @@ class UUser:
 
     @with_database('uyu_core')
     def load_user_by_login_name(self, login_name):
-        record = self.db.select_one("auth_user", {"login_name": login_name})
+        # record = self.db.select_one("auth_user", {"login_name": login_name})
+        sql = "select * from auth_user where login_name='%s' or nick_name='%s'" % (login_name, login_name)
+        record = self.db.get(sql)
         log.debug('#record: %s', record)
         if record:
             for key in self.ukey:
